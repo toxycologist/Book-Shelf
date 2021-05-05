3d202f594e776567844b71eef77d845583b24d1e
Marcin Kiepura <marcin.dagger@gmail.com>
2021-05-05 14:21:22 +0200

    Added Spring Security Authorities Roles Secured endpoints

--- src/main/java/pl/kiepura/Book/Shelf/repo/BookRepo.java
+++ src/main/java/pl/kiepura/Book/Shelf/repo/BookRepo.java
@@ -1,9 +1,19 @@
 package pl.kiepura.Book.Shelf.repo;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import pl.kiepura.Book.Shelf.dto.AuthorDTO;
 import pl.kiepura.Book.Shelf.entity.Book;
+
+import java.util.List;
 
 @Repository
 public interface BookRepo extends CrudRepository<Book, Long> {
+
+
+    @Query(value = "SELECT DISTINCT author FROM book", nativeQuery = true)
+    List<AuthorDTO> getAuthors();
+
+
 }
